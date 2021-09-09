e49ee3adc63c9c18edcf5aa684323278136af5df
tvoridobro <timerbulatov_danilka@mail.ru>
2021-09-09 23:24:06 +0300

    Testing add

---
+++ README.md
@@ -0,0 +1,8 @@
+Some data structures in go, using generics  
+To run use
+```
+go install golang.org/dl/gotip@latest
+gotip download
+gotip run <filename>
+```
+some examples of usage in test files
--- list/list_test.go
+++ list/list_test.go
@@ -1,14 +1,101 @@
 package list
 
 import (
-	"log"
 	"math/rand"
 	"reflect"
 	"testing"
 )
 
-func TestList_GetAll(t *testing.T) {
-	const length = 100
+func TestList_PeekPanic(t *testing.T) {
+	list := New[int]()
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("The code did not panic")
+		}
+	}()
+	list.Peek(0)
+}
+
+func TestList_PeekPanic2(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("The code did not panic")
+		}
+	}()
+	list := New[int]()
+	for i := 0; i < 20; i++ {
+		list.PushBack(i)
+	}
+	list.Peek(30)
+}
+
+func TestList_PopBack(t *testing.T) {
+	list := New[int]()
+	for i := 0; i < 20; i++ {
+		list.PushBack(i)
+	}
+	for i := 19; i >= 0; i-- {
+		if v := list.PopBack(); v != i {
+			t.Errorf("Wrong value got %d expected %d", v, i)
+		}
+	}
+}
+
+func TestList_PopFront(t *testing.T) {
+	list := New[int]()
+	for i := 0; i < 20; i++ {
+		list.PushFront(i)
+	}
+	for i := 19; i >= 0; i-- {
+		if v := list.PopFront(); v != i {
+			t.Errorf("Wrong value got %d expected %d", v, i)
+		}
+	}
+}
+
+func TestList_PopBackPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+	list := New[int]()
+	list.PopBack()
+}
+
+func TestList_PopFrontPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("The code did not panic")
+		}
+	}()
+	list := New[int]()
+	list.PopFront()
+}
+
+func TestList_Len(t *testing.T) {
+	list := New[int]()
+	if list.Len() != 0 {
+		t.Error("wrong length size")
+	}
+	list.PushFront(7)
+	if list.Len() != 1 {
+		t.Error("wrong length size")
+	}
+	list.PopBack()
+	if list.Len() != 0 {
+		t.Error("wrong length size")
+	}
+	for i := 0; i < 15; i++ {
+		list.PushBack(0)
+	}
+	if list.Len() != 15 {
+		t.Error("wrong length size")
+	}
+}
+
+func TestList_GetAllAndPeek(t *testing.T) {
+	length := 100
 	data := make([]int, 0)
 	list := New[int]()
 	for i := 0; i < length; i++ {
@@ -25,10 +112,10 @@ func TestList_GetAll(t *testing.T) {
 	}
 	for i := 0; i < length; i++ {
 		if data[i] != list.Peek(i) {
-			t.Fatalf("data[i] != list.Peek(i) %d %d", data[i], list.Peek(i))
+			t.Errorf("data[i] != list.Peek(i) %d %d", data[i], list.Peek(i))
 		}
 	}
 	if !reflect.DeepEqual(data, list.GetAll()) {
-		log.Fatal(data,list.GetAll())
+		t.Error(data, list.GetAll())
 	}
 }
