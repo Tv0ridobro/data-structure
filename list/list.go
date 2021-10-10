8c69395718431680761deb3e3883cb8165ee2169
Данил <Timerbulatov_danilka@mail.ru>
2021-10-10 14:20:29 +0300

    list now is doubly linked list, not a single add package documentation

--- list/list.go
+++ list/list.go
@@ -1,59 +1,73 @@
+// Package list implements a doubly linked list
+// See https://en.wikipedia.org/wiki/Linked_list for more details
 package list
 
 import (
 	"fmt"
 )
 
-//List is simple linked list
+// List represents a doubly linked list
+// Zero value of List is empty list
 type List[T any] struct {
 	head *Node[T]
+	tail *Node[T]
 	len  int
 }
 
-//New returns new list
+// New returns an initialized list
 func New[T any]() *List[T] {
 	return &List[T]{}
 }
 
-//PushFront adds data to the beginning of the list
+// PushFront adds value to the beginning of the list
 func (l *List[T]) PushFront(value T) {
 	l.len++
 	nn := &Node[T]{
 		value: value,
-		next:  nil,
 	}
 	if l.head == nil {
 		l.head = nn
 		return
 	}
+	if l.tail == nil { // 1 element in list
+		l.tail = l.head
+		l.head = nn
+		l.tail.prev = l.head
+		l.head.next = l.tail
+		return
+	}
 	nn.next = l.head
+	l.head.prev = nn
 	l.head = nn
 }
 
-//PushBack adds data to the end of the list
+// PushBack adds value to the end of the list
 func (l *List[T]) PushBack(value T) {
 	l.len++
-	newNode := &Node[T]{
+	nn := &Node[T]{
 		value: value,
-		next:  nil,
 	}
 	if l.head == nil {
-		l.head = newNode
+		l.head = nn
 		return
 	}
-	it := l.head
-	for it.next != nil {
-		it = it.next
+	if l.tail == nil { // 1 element in list
+		l.tail = nn
+		nn.prev = l.head
+		l.head.next = l.tail
+		return
 	}
-	it.next = newNode
+	l.tail.next = nn
+	nn.prev = l.tail
+	l.tail = nn
 }
 
-//Len returns length of the list
+// Len returns length of the list
 func (l *List[T]) Len() int {
 	return l.len
 }
 
-//GetAll returns all data from list from beginning to end
+// GetAll returns all elements from the list
 func (l *List[T]) GetAll() []T {
 	data := make([]T, 0, l.len)
 	it := l.head
@@ -64,130 +78,193 @@ func (l *List[T]) GetAll() []T {
 	return data
 }
 
-//PopBack removes data from the end of the list
-//panic if list is empty
+// PopBack removes last element of the list
+// Panics if list is empty
 func (l *List[T]) PopBack() T {
 	if l.len == 0 {
 		panic("empty list")
 	}
 	l.len--
-	prev := l.head
-	it := prev.next
-	if it == nil {
+	if l.tail == nil {
+		v := l.head.value
 		l.head = nil
-		return prev.value
+		return v
 	}
-	for it.next != nil {
-		prev = it
-		it = it.next
+	v := l.tail.value
+	l.tail.prev.next = nil
+	l.tail = l.tail.prev
+	if l.tail == l.head { // if 1 element in list
+		l.tail = nil
 	}
-	prev.next = nil
-	return it.value
+	return v
 }
 
-//PopFront removes data from the beginning of the list
-//panic if list is empty
+// PopFront removes first element of the list
+// Panics if list is empty
 func (l *List[T]) PopFront() T {
 	if l.len == 0 {
 		panic("empty list")
 	}
 	l.len--
-	data := l.head.value
+	v := l.head.value
 	l.head = l.head.next
-	return data
+	if l.head == nil {
+		return v
+	}
+	l.head.prev = nil
+	if l.head == l.tail {
+		l.tail = nil
+	}
+	return v
 }
 
-//ChangeAt changes value at given index
-//panic if list is empty
+// ChangeAt changes value at given index
+// panic if list is empty
 func (l *List[T]) ChangeAt(i int, value T) {
-	if i >= l.len {
-		panic(fmt.Sprintf("index higher than len %d %d", i, l.len))
-	}
-	it := l.head
-	counter := 0
-	for counter != i {
-		it = it.next
-		counter++
-	}
-	it.value = value
+	l.Node(i).value = value
 }
 
-//Peek returns element at the given index
-//panic if index is less or equal to len of the list
+// Peek returns element at the given index
+// Panics if index is less or equal to len of the list
 func (l *List[T]) Peek(i int) T {
 	return l.Node(i).value
 }
 
-//Node returns Node at the given index
-//panic if index is less or equal to len of the list
+// Node returns Node at the given index
+// Panics if index is less or equal to len of the list
 func (l *List[T]) Node(i int) *Node[T] {
 	if i >= l.len {
 		panic(fmt.Sprintf("index higher than len %d %d", i, l.len))
 	}
-	it := l.head
+	if i <= l.len/2 {
+		it := l.head
+		counter := 0
+		for counter != i {
+			it = it.next
+			counter++
+		}
+		return it
+	}
+	it := l.tail
 	counter := 0
-	for counter != i {
-		it = it.next
+	for counter != l.len-1-i {
+		it = it.prev
 		counter++
 	}
 	return it
 }
 
-//InsertAfter adds value after given node
-func (l *List[T]) InsertAfter(node *Node[T], value T) {
-	newNode := &Node[T]{
-		value: value,
-		next:  nil,
-	}
-	next := node.next
-	node.next = newNode
-	newNode.next = next
-	l.len++
-}
-
-//Merge adds given list at the end of initial
-//don't use second list after merging
+// Merge merges two lists
 func (l *List[T]) Merge(other *List[T]) {
+	if other.head == nil {
+		return
+	}
 	if l.head == nil {
 		l.len = other.len
 		l.head = other.head
+		l.tail = other.tail
 		return
 	}
-	it := l.head
-	for it.next != nil {
-		it = it.next
-	}
-	it.next = other.head
 	l.len += other.len
+	if l.tail == nil {
+		l.head.next = other.head
+		other.head.prev = l.head
+	} else {
+		l.tail.next = other.head
+		other.head.prev = l.tail
+	}
+	if other.tail == nil {
+		l.tail = other.head
+	} else {
+		l.tail = other.tail
+	}
 }
 
-//Clear set len 0 and head to nil
+// Clear clears list
 func (l *List[T]) Clear() {
 	l.len = 0
 	l.head = nil
+	l.tail = nil
 }
 
-//Reverse reverse given list
+// Reverse reverses given list
 func (l *List[T]) Reverse() {
-	if l.head == nil {
+	if l.head == nil || l.tail == nil {
 		return
 	}
 	var prev *Node[T]
-	it := l.head
+	it, f := l.head, l.head
 	next := it.next
 	for next != nil {
 		it.next = prev
+		it.prev = next
 		prev, it, next = it, next, next.next
 	}
 	it.next = prev
 	l.head = it
+	l.tail = f
 }
 
-//RemoveAfter remove next Node of given Node from List
-func (l *List[T]) RemoveAfter(n *Node[T]) {
-	if n.next == nil {
+// InsertAfter adds new Node with given value after given Node
+func (l *List[T]) InsertAfter(node *Node[T], value T) {
+	if node == l.tail || l.len == 1 {
+		l.PushBack(value)
 		return
 	}
-	n.next = n.next.next
+	l.len++
+	nn := &Node[T]{
+		value: value,
+	}
+	next := node.next
+	node.next = nn
+	nn.next = next
+	nn.prev = node
+	next.prev = nn
+}
+
+// RemoveAfter removes next Node of given Node from List
+func (l *List[T]) RemoveAfter(n *Node[T]) T {
+	if n == l.tail || n.next == nil {
+		panic("")
+	}
+	if n.next == l.tail {
+		return l.PopBack()
+	}
 	l.len--
+	next := n.next
+	gnext := next.next
+	gnext.prev = n
+	n.next = gnext
+	return next.value
+}
+
+// RemoveBefore removes previous Node of given Node from list
+func (l *List[T]) RemoveBefore(n *Node[T]) T {
+	if n == l.head {
+		panic("")
+	}
+	if n.prev == l.head {
+		return l.PopFront()
+	}
+	l.len--
+	prev := n.prev
+	gprev := prev.prev
+	gprev.next = n
+	n.prev = gprev
+	return prev.value
+}
+
+// InsertBefore adds new Node with given value before given Node
+func (l *List[T]) InsertBefore(n *Node[T], value T) {
+	if n == l.head {
+		l.PushFront(value)
+		return
+	}
+	l.len--
+	nn := &Node[T]{value: value}
+	prev := n.prev
+	prev.next = nn
+	nn.prev = prev
+	nn.next = n
+	n.prev = nn
 }
--- list/node.go
+++ list/node.go
@@ -1,28 +1,40 @@
 package list
 
+// Node represents node of a list
 type Node[T any] struct {
 	value T
 	next  *Node[T]
+	prev  *Node[T]
 }
 
-//Next return next Node
+// Next returns next Node
 func (n *Node[T]) Next() *Node[T] {
 	return n.next
 }
 
-//Value return value in Node
+// Prev returns previous Node
+func (n *Node[T]) Prev() *Node[T] {
+	return n.prev
+}
+
+// Value return value in Node
 func (n *Node[T]) Value() T {
 	return n.value
 }
 
-//Replace old value to new one and return old one
+// Replace replaces old value to new one and return old one
 func (n *Node[T]) Replace(value T) T {
 	old := n.value
 	n.value = value
 	return old
 }
 
-//HasNext return true if next node exists
+// HasNext return true if next node exists
 func (n *Node[T]) HasNext() bool {
 	return n.next != nil
+}
+
+// HasPrev return true if previous node exists
+func (n *Node[T]) HasPrev() bool {
+	return n.prev != nil
 }
