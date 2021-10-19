37626735d940c6161b829a73d0ca3e4194202c5b
Данил <Timerbulatov_danilka@mail.ru>
2021-10-19 13:09:14 +0300

    Add sparse table implementation

---
+++ sparse-table/sparse-table.go
@@ -0,0 +1,45 @@
+// Package sparse_table implements a sparse table
+package sparse_table
+
+import (
+	"fmt"
+	"github.com/Tv0ridobro/data-structure/util"
+)
+
+// SparseTable represents a sparse table
+// Zero value of SparseTable is invalid sparse table, should be used only with New()
+type SparseTable[T any] struct {
+	op       func(T, T) T
+	elements [][]T
+}
+
+// New returns an initialized sparse table
+// op should be idempotent, commutative and associative
+func New[T any](op func(T, T) T, elements []T) *SparseTable[T] {
+	ts := make([][]T, len(elements))
+	lg := util.Log2(len(ts))
+	for i := 0; i < len(ts); i++ {
+		ts[i] = make([]T, lg+1)
+		ts[i][0] = elements[i]
+	}
+	for j := 1; j <= util.Log2(len(elements)); j++ {
+		for i := 0; i < len(elements); i++ {
+			if (i + (1 << (j - 1))) < len(elements) {
+				ts[i][j] = op(ts[i][j-1], ts[i+(1<<(j-1))][j-1])
+			}
+		}
+	}
+	return &SparseTable[T]{
+		op:       op,
+		elements: ts,
+	}
+}
+
+// Query returns result of operation on elements[l:r+1]
+func (s *SparseTable[T]) Query(l, r int) T {
+	if l > r || l < 0 || r >= len(s.elements) {
+		panic(fmt.Sprintf("wrong indices %d %d", l, r))
+	}
+	log := util.Log2(r - l + 1)
+	return s.op(s.elements[l][log], s.elements[r-(1<<log)+1][log])
+}
---
+++ sparse-table/sparse-table_test.go
@@ -0,0 +1,22 @@
+package sparse_table
+
+import (
+	"github.com/Tv0ridobro/data-structure/util"
+	"testing"
+)
+
+func TestSparseTable_Query(t *testing.T) {
+	table := New(util.GCD, []int{2, 3, 5, 4, 6, 8, 16})
+	if v := table.Query(0, 2); v != 1 {
+		t.Errorf("wrong answer for query %d %d", v, 1)
+	}
+	if v := table.Query(3, 5); v != 2 {
+		t.Errorf("wrong answer for query %d %d", v, 2)
+	}
+	if v := table.Query(2, 3); v != 1 {
+		t.Errorf("wrong answer for query %d %d", v, 1)
+	}
+	if v := table.Query(5, 6); v != 8 {
+		t.Errorf("wrong answer for query %d %d", v, 8)
+	}
+}
