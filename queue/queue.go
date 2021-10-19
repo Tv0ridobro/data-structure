2baefb85664106b1ccd85d527d128ea4b084baac
Данил <Timerbulatov_danilka@mail.ru>
2021-10-19 14:51:10 +0300

    Queue rework
    Add back and Front methods
    Add package documentation

--- queue/queue.go
+++ queue/queue.go
@@ -1,19 +1,41 @@
+// Package queue implements a doubly linked list
+// See https://en.wikipedia.org/wiki/Queue_(abstract_data_type) for more details
 package queue
 
 import "github.com/Tv0ridobro/data-structure/list"
 
+// Queue represents a queue
+// Zero value of Stack is invalid stack, should be used only with New()
 type Queue[T any] struct {
-	list list.List[T]
+	list *list.List[T]
 }
 
+// New returns an initialized queue
+func New[T any]() *Queue[T] {
+	return &Queue[T]{list: list.New[T]()}
+}
+
+// Enqueue adds element to the end of queue
 func (q *Queue[T]) Enqueue(value T) {
 	q.list.PushBack(value)
 }
 
+// Dequeue removes element from head of queue
 func (q *Queue[T]) Dequeue() T {
 	return q.list.PopFront()
 }
 
+// Back returns last element of queue
+func (q *Queue[T]) Back() T {
+	return q.list.Back()
+}
+
+// Front returns first element of queue
+func (q *Queue[T]) Front() T {
+	return q.list.Front()
+}
+
+// Size returns size of queue
 func (q *Queue[T]) Size() int {
 	return q.list.Len()
 }
---
+++ queue/queue_test.go
@@ -0,0 +1,95 @@
+package queue
+
+import "testing"
+
+func TestQueue_Front(t *testing.T) {
+	q := New[int]()
+	q.Enqueue(0)
+	q.Enqueue(1)
+	q.Enqueue(2)
+	q.Enqueue(3)
+	q.Enqueue(4)
+	if v := q.Front(); v != 0 {
+		t.Errorf("front wrong value %d != %d", v, 0)
+	}
+	q.Dequeue()
+	if v := q.Front(); v != 1 {
+		t.Errorf("front wrong value %d != %d", v, 1)
+	}
+	q.Dequeue()
+	if v := q.Front(); v != 2 {
+		t.Errorf("front wrong value %d != %d", v, 2)
+	}
+	q.Dequeue()
+	if v := q.Front(); v != 3 {
+		t.Errorf("front wrong value %d != %d", v, 3)
+	}
+	q.Dequeue()
+	if v := q.Front(); v != 4 {
+		t.Errorf("front wrong value %d != %d", v, 4)
+	}
+}
+
+func TestQueue_Dequeue(t *testing.T) {
+	q := New[int]()
+	q.Enqueue(0)
+	q.Enqueue(1)
+	q.Enqueue(2)
+	q.Enqueue(3)
+	q.Enqueue(4)
+	if v := q.Dequeue(); v != 0 {
+		t.Errorf("front wrong value %d != %d", v, 0)
+	}
+	if v := q.Dequeue(); v != 1 {
+		t.Errorf("front wrong value %d != %d", v, 1)
+	}
+	if v := q.Dequeue(); v != 2 {
+		t.Errorf("front wrong value %d != %d", v, 2)
+	}
+	if v := q.Dequeue(); v != 3 {
+		t.Errorf("front wrong value %d != %d", v, 3)
+	}
+	if v := q.Dequeue(); v != 4 {
+		t.Errorf("front wrong value %d != %d", v, 4)
+	}
+}
+
+func TestQueue_Back(t *testing.T) {
+	q := New[int]()
+	q.Enqueue(0)
+	if v := q.Back(); v != 0 {
+		t.Errorf("front wrong value %d != %d", v, 0)
+	}
+	q.Enqueue(1)
+	if v := q.Back(); v != 1 {
+		t.Errorf("front wrong value %d != %d", v, 1)
+	}
+	q.Enqueue(2)
+	if v := q.Back(); v != 2 {
+		t.Errorf("front wrong value %d != %d", v, 2)
+	}
+	q.Enqueue(3)
+	if v := q.Back(); v != 3 {
+		t.Errorf("front wrong value %d != %d", v, 3)
+	}
+	q.Enqueue(4)
+	if v := q.Back(); v != 4 {
+		t.Errorf("front wrong value %d != %d", v, 4)
+	}
+}
+
+func TestQueue_Size(t *testing.T) {
+	q := New[int]()
+	q.Enqueue(0)
+	q.Enqueue(1)
+	q.Enqueue(2)
+	q.Enqueue(3)
+	q.Dequeue()
+	q.Enqueue(4)
+	q.Dequeue()
+	q.Dequeue()
+	q.Enqueue(1)
+	if v := q.Size(); v != 3 {
+		t.Errorf("wrong size %d != %d", v, 3)
+	}
+}
