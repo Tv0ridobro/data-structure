7d5d52b6280021e82ca0bbff463c600e26004ee7
Данил <Timerbulatov_danilka@mail.ru>
2021-10-23 15:08:33 +0300

    Add new function NextPermutation

---
+++ algorithm/combinatorics/combinatorics.go
@@ -0,0 +1,25 @@
+package combinatorics
+
+import (
+	"github.com/Tv0ridobro/data-structure/util"
+)
+
+// NextPermutation returns next permutation
+func NextPermutation(s []int) []int {
+	next := make([]int, len(s))
+	copy(next, s)
+	for i := len(s) - 2; i >= 0; i-- {
+		if next[i] < next[i+1] {
+			z := i + 1
+			for j := i + 1; j < len(s); j++ {
+				if next[j] >= next[i] && next[j] <= next[z] {
+					z = j
+				}
+			}
+			next[i], next[z] = next[z], next[i]
+			next = append(next[:i+1], util.Reverse(next[i+1:])...)
+			break
+		}
+	}
+	return next
+}
---
+++ algorithm/combinatorics/combinatorics_test.go
@@ -0,0 +1,23 @@
+package combinatorics
+
+import (
+	"github.com/Tv0ridobro/data-structure/util"
+	"testing"
+)
+
+func TestNextPermutation(t *testing.T) {
+	tests := []struct {
+		now  []int
+		next []int
+	}{
+		{[]int{1, 2, 3}, []int{1, 3, 2}},
+		{[]int{5, 4, 3, 2, 1}, []int{5, 4, 3, 2, 1}}, // no next permutation
+		{[]int{1, 2, 3, 4, 5, 6}, []int{1, 2, 3, 4, 6, 5}},
+		{[]int{}, []int{}},
+		{[]int{3, 9, 8, 7, 6, 5, 4, 2, 1}, []int{4, 1, 2, 3, 5, 6, 7, 8, 9}}}
+	for i := range tests {
+		if v := NextPermutation(tests[i].now); !util.Equal(v, tests[i].next) {
+			t.Errorf("wrong next perutation %v != %v", v, tests[i].next)
+		}
+	}
+}
